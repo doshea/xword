6bb7473006f744de86f193441ab5f0bfe1b12be1
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-05-15 22:14:53 -0400

    Trying to precompile edit.js and solve.js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,8 +48,8 @@ Xword::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( welcome.js )
   config.assets.precompile += %w( welcome.css )
-  config.assets.precompile += %w( edit.js )
-  config.assets.precompile += %w( solve.js )
+  config.assets.precompile += %w( crosswords/edit.js )
+  config.assets.precompile += %w( crosswords/solve.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
