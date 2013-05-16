30abf9a2b3ff14b355198a82e7f7be9b786ca50f
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-05-15 22:27:18 -0400

    Moved edit and solve.js out of the crosswords folder to try to help precompilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,10 +46,7 @@ Xword::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( welcome.js )
-  config.assets.precompile += %w( welcome.css )
-  config.assets.precompile += %w( edit.js )
-  config.assets.precompile += %w( solve.js )
+  config.assets.precompile += %w( welcome.js welcome.css edit.js solve.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
