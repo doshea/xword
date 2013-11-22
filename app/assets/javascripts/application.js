5bfd456d9bd05b93289c9dcca65bba925d79875d
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-11-22 00:23:22 -0800

    Upgrading gems and spacing some of the CSS and JS dependencies, preparing for Foundation 5 attempt

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,8 +3,11 @@
 //= require jquery_ujs
 // require turbolinks
 //= require underscore-min
+//
 //= require foundation.min
+//
 //= require custom.modernizr
+//
 //= require moment.min
 //= require sugar.min
 //= require gritter
