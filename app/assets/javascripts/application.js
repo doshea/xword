4fffecf0889e75cbc78280f03f56a3d8e6c2a356
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-11-23 00:16:19 -0800

    Switching to Foundation 5

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,12 +1,16 @@
 //
-//= require jquery
+// require jquery
+//= require foundation5/jquery
 //= require jquery_ujs
+//
 // require turbolinks
 //= require underscore-min
 //
-//= require foundation.min
+// require foundation5/foundation.min
+// require foundation4/foundation.min
 //
-//= require custom.modernizr
+// require foundation5/modernizr
+// require foundation4/custom.modernizr
 //
 //= require moment.min
 //= require sugar.min
