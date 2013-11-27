41b8c4732092b258ea8aae7b50e89c1304203407
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-11-27 04:21:54 -0800

    Adding show pages for words and clues

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -6,5 +6,12 @@ class WordsController < ApplicationController
   end
   def show
     @word = Word.find(params[:id])
+
+    across_crosswords = @word.across_crosswords
+    down_crosswords = @word.down_crosswords
+    @count = across_crosswords.length + down_crosswords.length
+    @crosswords = (across_crosswords + down_crosswords).uniq.sort{|x,y| x.title <=> y.title}
+
+    @clues = @word.clues.sort{|x,y| x.difficulty <=> y.difficulty}
   end
 end
