c08b3b5c66cb0f85e61679b5ceec80bf412fc3e9
Dylan O'Shea <dylan.j.oshea@gmail.com>
2014-05-23 12:34:14 -0700

    Using Coffeescript for cell_funcs, changing old CSS to underscore casing, adding feature testing for login from welcome page

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,6 +1,44 @@
 feature 'Login' do
-  scenario 'Anonymous user logs in from home page' do
-    visit root_path
-    click_link 'Log In'
+
+  before :each do
+    create(:min_user)
   end
+
+  scenario 'Anonymous users are redirected from root to welcome page' do
+    visit root_path
+    current_path.should eq welcome_path
+  end
+
+  scenario 'Anonymous users can log in' do
+    visit root_path
+    page.should have_selector('#login-container')
+
+    within('#login-container') do
+      fill_in :username, with: 'min_user'
+      fill_in :password, with: 'abcde'
+      click_button 'Log in'
+    end
+
+    visit root_path
+    current_path.should eq root_path
+    page.should_not have_selector('#login-container')
+    expect(page).to have_text("Welcome back")
+
+  end
+
+  scenario 'Signing in from welcome page with bad credentials redirects to welcome page' do
+    visit root_path
+
+    within('#login-container') do
+      fill_in :username, with: 'min_user'
+      fill_in :password, with: 'wrong'
+      click_button 'Log in'
+    end
+
+    visit root_path
+    current_path.should eq welcome_path
+    page.should have_selector('#login-container')
+    expect(page).to_not have_text("Welcome back")
+  end
+
 end
