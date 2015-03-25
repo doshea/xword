157a6203a797259ce0c0abc23ee8af268bf0ed36
Dylan O'Shea <dylan.j.oshea@gmail.com>
2015-03-25 14:36:15 -0700

    removing traces of published from publishable

--- app/models/concerns/publishable.rb
+++ app/models/concerns/publishable.rb
@@ -2,13 +2,12 @@ module Publishable
   extend ActiveSupport::Concern
 
   included do
-    default_scope {order(published_at: :desc)}
     scope :standard, -> {where(rows: 15, cols: 15)}
     scope :nonstandard, -> {where.not('(rows = 15) AND (cols = 15)')}
 
-    scope :solved, -> (user_id) {joins(:solutions).where(solutions: {user_id: user_id, is_complete: true}).published.distinct}
+    scope :solved, -> (user_id) {joins(:solutions).where(solutions: {user_id: user_id, is_complete: true}).distinct}
     scope :in_progress, -> (user_id) {joins(:solutions).where(solutions: {user_id: user_id, is_complete: false}).distinct}
-    scope :unstarted, -> (user_id) {joins(:solutions).where.not(solutions: {user_id: user_id}).published.distinct}
+    scope :unstarted, -> (user_id) {joins(:solutions).where.not(solutions: {user_id: user_id}).distinct}
 
     scope :solo, -> {where(solutions: {team: false})}
     scope :teamed, -> {where(solutions: {team: true})}
--- spec/factories/crossword_factory.rb
+++ spec/factories/crossword_factory.rb
@@ -2,19 +2,17 @@
 #
 # Table name: crosswords
 #
-#  id           :integer          not null, primary key
-#  title        :string(255)      default("Untitled"), not null
-#  letters      :text             default(""), not null
-#  description  :text
-#  rows         :integer          default(15), not null
-#  cols         :integer          default(15), not null
-#  published    :boolean          default(FALSE), not null
-#  published_at :datetime
-#  user_id      :integer
-#  created_at   :datetime
-#  updated_at   :datetime
-#  circled      :boolean          default(FALSE)
-#  preview      :text
+#  id          :integer          not null, primary key
+#  title       :string(255)      default("Untitled"), not null
+#  letters     :text             default(""), not null
+#  description :text
+#  rows        :integer          default(15), not null
+#  cols        :integer          default(15), not null
+#  user_id     :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#  circled     :boolean          default(FALSE)
+#  preview     :text
 #
 
 FactoryGirl.define do
